d19dd03d0ee83967305cc2d3b70e09923a9de958
yuri_rodnov <somewaystodo@yandex.ru>
2026-04-20 00:05:39 +0300

    add number  guesser

---
+++ 002_guess_number/guess_number.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+
+
+	var win int = 0
+	var lose int = 0
+	var try int = 0
+	
+	var min int = 1
+	var max int = 5
+
+
+	fmt.Println("Welcome to Guessing number game!")
+	fmt.Printf("Guess number from %d to %d:\n", min, max)
+	fmt.Println("Push Ctrl + C to stop game")
+
+	for {
+		var userAnswer int
+		var randomNumber int = rand.Intn(max - min + 1) + min
+
+		fmt.Printf("==== Try №%d ====\n", try)
+		fmt.Print("Enter number: ")
+		
+		_, err := fmt.Scanln(&userAnswer)
+
+		if err != nil{
+			fmt.Println("Please, type a valid number")
+			fmt.Scanln()
+			try += 1
+			continue
+		}
+
+		if userAnswer == randomNumber{
+			win +=1
+			try += 1
+			fmt.Printf("Great! %d was guessed\n", randomNumber)			
+		} else if(userAnswer < min || userAnswer > max) {
+			fmt.Printf("Please, type number between %d and %d\n", min, max)
+			try += 1
+		} else {
+			lose += 1 
+			try += 1
+			fmt.Printf("You lose! %d was guessed\n", randomNumber)			
+		}
+
+		fmt.Printf("Score: Try %d | Wins %d | Loses: %d\n", try, win, lose)
+				
+	}
+
+}
