a4e399d846aa393d13fe7a497362f0b3674ecfd5
yuri_rodnov <somewaystodo@yandex.ru>
2026-06-19 23:33:41 +0300

    add map

--- 005_rename_files/rename_files.go
+++ 005_rename_files/rename_files.go
@@ -2,10 +2,27 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"os"
 )
 
-func main() {
+
+
+var NEW_NAME_LENGTH = 20;
+var NEW_NAME_SYMBOLS_SET = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+func createRandomName() string {
+		var newName string;		
+
+		for i := 0; i < NEW_NAME_LENGTH; i++ {
+			randIndex := rand.Intn(len(NEW_NAME_SYMBOLS_SET))
+			newName += string(NEW_NAME_SYMBOLS_SET[randIndex])
+		}
+
+		return newName
+}
+
+func main() {	
 	if len(os.Args) < 2{
 		fmt.Println("Need path-dir argument!")
 		return
@@ -13,24 +30,37 @@ func main() {
 
 	path := os.Args[1];
 
-
 	fileInfo, err := os.Stat(path);
-
 	if(err != nil){
 		fmt.Printf("%s is not a path", path);
 		return
-	}
-
-	
+	}	
 
 	if(!fileInfo.IsDir()){
 		fmt.Printf("%s is not a directory", fileInfo)
 		return	
 	}
 
-	fmt.Println("Ok")
+	filesList, err := os.ReadDir(path)
+	if(err != nil){
+		fmt.Printf("Error while reading %s", path)
+	}
 
-	//fmt.Printf("Yor arg is %s", os.Args[1]);
+	namesMap := make(map[string]string)
+
+	for _, file := range filesList {
+		fmt.Println(file)
+		newName := createRandomName();
+		namesMap[file.Name()] = newName
+	}
+
+
+	fmt.Println(namesMap)
+
+
+	
+
+	
 
 
 
