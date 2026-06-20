28a451e214bde776d5a9df73de1940e6c4b22b6f
yuri_rodnov <somewaystodo@yandex.ru>
2026-06-20 17:45:46 +0300

    make renaming

--- 005_rename_files/rename_files.go
+++ 005_rename_files/rename_files.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"math/rand"
 	"os"
+	"path/filepath"
 )
-
-
 
 var NEW_NAME_LENGTH = 20;
 var NEW_NAME_SYMBOLS_SET = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
@@ -46,16 +46,38 @@ func main() {
 		fmt.Printf("Error while reading %s", path)
 	}
 
-	namesMap := make(map[string]string)
+	fmt.Printf("Prcessing files in %s\n", path)
 
+	namesMap := make(map[string]string)
+	
 	for _, file := range filesList {
-		fmt.Println(file)
+		fmt.Printf("Renaming file %s \n", file.Name())
+		
+		
+		fileExt := filepath.Ext(file.Name())
 		newName := createRandomName();
+		oldPath := filepath.Join(path, file.Name())
+		newPath := filepath.Join(path, newName + fileExt)
+
+
 		namesMap[file.Name()] = newName
+		
+
+		err := os.Rename(oldPath, newPath)
+
+		if(err != nil){
+			log.Fatal(err)
+		}
+
+		fmt.Printf("Renamed: %s -> %s \n", file.Name(), newName + fileExt)
+		
+		// fmt.Println(oldPath)
+		// fmt.Println(newPath)
+		// fmt.Println("-----")
 	}
 
 
-	fmt.Println(namesMap)
+	
 
 
 	
